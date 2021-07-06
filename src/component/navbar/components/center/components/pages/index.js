79629c98131cf5201f1108600859b6074d9e6136
5no0p <mmoaaa1989@hotmail.com>
2021-07-06 13:06:07 +0200

    2 files modified

--- src/component/navbar/components/center/components/pages/index.js
+++ src/component/navbar/components/center/components/pages/index.js
@@ -4,27 +4,102 @@ const Pages = () =>{
   const Home = {
     name:"Home",
     isDropDown:true,
-    children:['item','item2','item3','item4']
+    children:[
+      {
+        isNested:false,
+        data:"item"
+      },
+      {
+        isNested:true,
+        data:"item",
+        nest:["nest1","nest2","nest3"]
+      },
+      {
+        isNested:true,
+        data:"item",
+        nest:["nest","nest1"]
+      }
+    ]
   }
   const Pages = {
     name:"Pages",
     isDropDown:true,
-    children:['Notitem','Notitem2','Notitem3','Notitem4']
+    children:[
+      {
+        isNested:false,
+        data:"item"
+      },
+      {
+        isNested:true,
+        data:"item",
+        nest:["nest1","nest2","nest3"]
+      },
+      {
+        isNested:true,
+        data:"item",
+        nest:["nest","nest1"]
+      }
+    ]
   }
   const Blog = {
     name:"Blog",
     isDropDown:true,
-    children:['Notitem','Notitem2','Notitem3','Notitem4']
+    children:[
+      {
+        isNested:false,
+        data:"item"
+      },
+      {
+        isNested:true,
+        data:"item",
+        nest:["nest1","nest2","nest3"]
+      },
+      {
+        isNested:true,
+        data:"item",
+        nest:["nest","nest1"]
+      }
+    ]
   }
   const Portfolio = {
     name:"Portfolio",
     isDropDown:true,
-    children:['Notitem','Notitem2','Notitem3','Notitem4']
+    children:[
+      {
+        isNested:false,
+        data:"item"
+      },
+      {
+        isNested:true,
+        data:"item",
+        nest:["nest1","nest2","nest3"]
+      },
+      {
+        isNested:true,
+        data:"item",
+        nest:["nest","nest1"]
+      }
+    ]
   }
   const Shop = {
     name:"Shop",
     isDropDown:true,
-    children:['Notitem','Notitem2','Notitem3','Notitem4']
+    children:[
+      {
+        isNested:false,
+        data:"item"
+      },
+      {
+        isNested:true,
+        data:"itemNest",
+        nest:["nest1","nest2","nest3"]
+      },
+      {
+        isNested:true,
+        data:"itemNest",
+        nest:["nest","nest1"]
+      }
+    ]
   }
   const Contact = {
     name:"Contacts",
@@ -67,14 +142,45 @@ const ListItems = ({info}) =>{
         </a>
         {info.isDropDown
         ?<ul class="top-0 w-48 bg-white shadow px-6 py-8 rounded-lg .dd-menu absolute" >
-           {info.children.map(children => <li class="py-1" key={children}><a class="font-bold text-base mb-2 cursor-pointer">{children}</a></li>)}
-          </ul> 
+           {info.children.map(children => children.isNested
+            ?<li class="py-1 nest" key={`${info.name}_${children.data}`}><a class="font-bold text-base mb-2 cursor-pointer" style={{display:"inline-block"}}><div className="static ">
+            <div className="dd-font font-bold text-base relative tracking-wider z-10 mb-0.5 px-1" style={{margin: "0.13rem"}}>
+              {children.data}
+            </div>
+            <div className={`-mt-3 static`} style={{height:"100%"}}>
+              <div className={`underline`}></div>
+            </div>
+            </div></a><listBox items={children.nest}/></li>
+            
+            :<li class="py-1" key={`${info.name}_${children.data}`}><a class="font-bold text-base mb-2 cursor-pointer" style={{display:"inline-block"}}><div className="static ">
+            <div className="dd-font font-bold text-base relative tracking-wider z-10 mb-0.5 px-1" style={{margin: "0.13rem"}}>
+              {children.data}
+            </div>
+            <div className={`-mt-3 static`} style={{height:"100%"}}>
+              <div className={`underline`}></div>
+            </div>
+            </div></a></li>
+            )}
+          </ul>
         :""
+  
         }     
       </li>
 
     </> 
  )
+}
+
+const listBox = ({items}) => {
+  
+  return (
+    <>
+      <ul class="top-0 w-48 bg-white shadow px-6 py-8 rounded-lg .dd-menu absolute" >
+           {items.map(children => <li class="py-1" key={children}><a class="font-bold text-base mb-2 cursor-pointer">{children}</a></li>)}
+          </ul>
+    </>
+  )
+
 }
 
 export default Pages
