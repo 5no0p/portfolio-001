6c129d55c37a75bf22f3dc99462c094031dd923f
5no0p <mmoaaa1989@hotmail.com>
2021-07-15 00:55:35 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./tailwind.css";
 import "tailwindcss/tailwind.css"
 import Nav from "./component/navbar"
 import Hero from "./component/hero"
+import About from "./component/about"
 
 export default function App() {
   return (
     <>
     <Nav/>
     <Hero/>
+    <About/>
     </>
   );
 }
--- src/component/about/index.js
+++ src/component/about/index.js
@@ -5,14 +5,32 @@ const About = () => {
 
   return (
     <>
-    
-
-
+    <div className="grid grid-cols-2">
+    <Image/>
+    <Content/>
+    </div>
     </>
   )
 
 }
 
+const Image = () => {
 
+  return (
+    <>
+      <div>
+        <img src="http://nimbus.wgl-demo.net/wp-content/uploads/2020/02/home_01-1.png"/>
+      </div>
+    </>
+  )
+}
+const Content = () => {
+
+  return (
+    <>
+      <div className="bg-blue-500">about me</div>
+    </>
+  )
+}
 
 export default About;
