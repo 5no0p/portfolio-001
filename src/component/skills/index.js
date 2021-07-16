6b3e415b2861d2e2c1529ccc0c737bebdb7b03bc
5no0p <mmoaaa1989@hotmail.com>
2021-07-16 16:21:44 +0200

    2 files modified

--- src/component/skills/index.js
+++ src/component/skills/index.js
@@ -5,10 +5,32 @@ const Skills = () => {
   
   return (
     <>
-    
+    <div className="skills grid grid-cols-2">
+    <Content />
+    <Skill/>
+    </div>
     </>
 
   )
 }
 
+const Content = () => {
+
+  return (
+
+    <>
+    <div className="bg-red-300">content</div>
+    </>
+  )
+}
+
+const Skill = () => {
+
+  return (
+    <>
+    <div className="bg-blue-300">skills</div>
+
+    </>
+  )
+}
 export default Skills;
