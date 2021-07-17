85f4ca22bc8368e4a2ac0e0f85f38ac4b1c8dfe1
5no0p <mmoaaa1989@hotmail.com>
2021-07-17 02:50:51 +0200

    3 files modified

--- src/component/skills/index.js
+++ src/component/skills/index.js
@@ -20,34 +20,43 @@ const Content = () => {
   return (
 
     <>
-      <div className="section-content grid grid-cols-12 flex" style={{marginLeft: "1.75rem",marginRight: "8rem"}}>
+      <div className="section-content skills-content grid grid-cols-12 flex mx-4">
         <div className="col-span-1">
-          <h5 className="vhead">about me</h5>
-          <div className="vl"></div>
+          <h5 className="vhead">my skills</h5>
+          <div className="vl" style={{left:"17%"}}></div>
         </div>
         <div className="col-span-11">
           <div className="section-header">
-            <span>Need Creative Design? I can Help You!</span>
+            <span>what my skills included</span>
           </div>
           <div className="my-3 section-paragraph">
-            <p>Product Designer and Digital Creative Director working in design field for 13 years so far, specialized in UI/UX, Branding and digital designs. These are the words we live by in everything we do. Every story we tell, every brand we build, and every interaction we create must not only look beautiful. beautifully, too.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tellus consectetur sit mi mattis sed integer lorem.</p>
           </div>
-          <div className="font-bold my-4 about-follow">
-          You Can Follow Me also Here:
-          </div>
-          <div className="flex space-x-5 my-5 about-social">
-            <a href="#" className="flex space-x-4 youtube-link">
-            <SocialIcon network="youtube"/>
-            <h1 className="uppercase font-bold flex items-center" style={{color:"#ff3333"}}>youtube</h1>
-            </a>
-            <a href="#" className="flex space-x-4 github-link">
-            <SocialIcon network="github" fgColor="#ffffff" bgColor="#24292e"/>
-            <h1 className="uppercase font-bold flex items-center" style={{color:"#24292e"}}>github</h1>
-            </a>
-            <a href="#" className="flex space-x-4 instagram-link">
-            <SocialIcon network="instagram" fgColor="#ffffff" bgColor="#FFC13E"/>
-            <h1 className="uppercase font-bold flex items-center" style={{color:"#FFC13E"}}>insta</h1>
-            </a>
+          <div className="">
+            <div className="relative my-4">
+              <div className="my-2 flex justify-between">
+              <h3 className="uppercase">javascript</h3>
+              <h3 className="uppercase">80%</h3>
+              </div>
+              <div style={{backgroundColor:"#ffae00",height:"10px",width:"80%",zIndex:"20"}}></div>
+              <div style={{borderTop: "1px solid #adadad",height:"1px",position:"relative",top:"-5px",zIndex:"10"}}></div>
+            </div>
+            <div className="relative my-4">
+              <div className="my-2 flex justify-between">
+              <h3 className="uppercase">Python</h3>
+              <h3 className="uppercase">90%</h3>
+              </div>
+              <div style={{backgroundColor:"#ffae00",height:"10px",width:"90%",zIndex:"20"}}></div>
+              <div style={{borderTop: "1px solid #adadad",height:"1px",position:"relative",top:"-5px",zIndex:"10"}}></div>
+            </div>
+            <div className="relative my-4">
+              <div className="my-2 flex justify-between">
+              <h3 className="uppercase">figma</h3>
+              <h3 className="uppercase">75%</h3>
+              </div>
+              <div style={{backgroundColor:"#ffae00",height:"10px",width:"75%",zIndex:"20"}}></div>
+              <div style={{borderTop: "1px solid #adadad",height:"1px",position:"relative",top:"-5px",zIndex:"10"}}></div>
+            </div>
           </div>
         </div>
       </div>
