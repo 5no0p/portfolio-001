5151b8c5dc437b039d35b8b6e683ecd4c7344bab
5no0p <mmoaaa1989@hotmail.com>
2021-07-21 22:47:27 +0200

    1 file modified

--- src/component/projects/index.js
+++ src/component/projects/index.js
@@ -5,8 +5,10 @@ const Projects = () => {
 
   return (
     <>
-      <Content/>
-      <Project/>
+      <div className="projects">
+        <Content/>
+        <Project/>
+      </div>
     </>
   )
 }
@@ -15,14 +17,14 @@ const Content = () => {
 
   return (
     <>
-      <div className="section-content skills-content grid grid-cols-12 flex mx-4" ref={ref}>
+      <div className="grid grid-cols-12 flex mx-4">
         <div className="col-span-1">
-          <h5 className="vhead">my skills</h5>
+          <h5 className="vhead">my works</h5>
           <div className="vl" style={{left:"17%"}}></div>
         </div>
         <div className="col-span-11">
           <div className="section-header">
-            <span>what my skills included</span>
+            <span>Meet My Projects</span>
           </div>
           <div className="my-3 section-paragraph">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tellus consectetur sit mi mattis sed integer lorem.</p>
